901f6233413bd2a6f3c073d6495f680f536c9872
ebamboo <1453810050@qq.com>
2024-01-25 18:13:22 +0800

    其他布局

--- SwiftUIAPP/Study/AlignmentView.swift
+++ SwiftUIAPP/Study/AlignmentView.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct AlignmentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            VStack(alignment: .leading, spacing: 8) { // item alignment
+                
+                
+                
+                Text("容器的 item alignment 是内部 item之间的对齐方式")
+                    .frame(width: 240)
+                    .background(.gray)
+                
+                Text("容器的 frame alignment 是实际内容相对占用的空间的对齐方式")
+                    .frame(width: 300)
+                    .background(.gray)
+                
+                Text("这是个 frame trading 对齐；item leading 对齐")
+                    .frame(width: 200, height: 80)
+                    .background(.gray)
+                
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 180, height: 80)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .trailing) // frame alignment
+            .background(.orange)
+            
+            VStack(alignment: .center) {
+                
+                Text("=============")
+                    .frame(width: 200, height: 60)
+                    .background(Color.orange)
+                
+                Text("=============")
+                    .frame(width: 200, height: 60)
+                    .background(Color.orange)
+                
+                ///
+                /// alignmentGuide 第一个参数必须是容器的对齐方式一样，否则忽略无效；第二个参数是一个闭包，闭包中有个参数，根据该参数提供的信息调整对齐偏移量
+                Text("100")
+                    .frame(width: 100, height: 60)
+                    .background(Color.orange)
+                    .alignmentGuide(HorizontalAlignment.center, computeValue: { dimension in
+                        return dimension[HorizontalAlignment.center] - dimension.width / 2
+                    })
+                
+                HStack(spacing: 0) {
+                    Rectangle().fill(.orange)
+                    Rectangle().fill(.yellow)
+                    Rectangle().fill(.gray)
+                    Rectangle().fill(.black)
+                    Rectangle().fill(.blue)
+                }
+                .frame(width: 300, height: 60)
+                .background(Color.orange)
+                
+                Text("100")
+                    .frame(width: 100, height: 60)
+                    .background(Color.orange)
+                    .alignmentGuide(HorizontalAlignment.center, computeValue: { dimension in
+                        return dimension[HorizontalAlignment.center] + dimension.width / 2
+                    })
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+            .background(.cyan)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.gray)
     }
 }
 
--- SwiftUIAPP/Study/GeometryView.swift
+++ SwiftUIAPP/Study/GeometryView.swift
@@ -9,7 +9,77 @@ import SwiftUI
 
 struct GeometryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            Divider()
+            GeometryReader { geometry in
+                ScrollView {
+                    
+                    VStack {
+                        
+                        let text1 = """
+                              主要处理两类问：
+                              1、按比例分配给子视图空间；
+                              2、设置特定子类的宽高比；
+                              """
+                        Text(text1)
+                            .background(.gray)
+                        
+                        let text2 = """
+                              1、使用 .aspectRatio(.init(width: 1, height: 4), contentMode: .fit) 来处理等比例视图问题；
+                              其中 size 表示宽高比例；结合 zstack，用 Rectangle()去撑起相应的尺寸；
+                              设置目标视图的 frame 最大值为无限大，把它放入刚才的 zstack 中；
+                              这个方案比 geometry 更像是 SwiftUI 方式更优雅；
+                              注意：也要保证目标视图不要超过 Rectangle() 撑起的尺寸，不然再次撑大 zstack；
+                              """
+                        Text(text2)
+                            .background(.gray)
+                        
+                        let text3 = """
+                              Geometry 没法根据自身子视图的布局信息跟新自己的布局信息，也即是由父视图容器直接分配给最大的空间；
+                              如果父视图没提供，则使用 idea 尺寸；
+                              """
+                        Text(text3)
+                            .background(.gray)
+                        
+                        Text("实现一个宽高比为 2:1 动态高度")
+                            .frame(width: geometry.size.width, height: geometry.size.width/2)
+                            .background(.gray)
+                        
+                        ZStack {
+                            Rectangle()
+                                .fill(.orange)
+                                .aspectRatio(.init(width: 2, height: 1), contentMode: .fit)
+                                .overlay {
+                                    Text("2:1")
+                                }
+                                
+                            // 目标视图
+                        }
+                        
+                        Text("position 会返回一个新的视图，这个视图会占用所有可用空间（它修饰的视图的父视图所能提供的空间），然后才会布局它修饰的视图。")
+                            .background(.gray)
+                        
+                        Rectangle()
+                            .fill(.orange)
+                            .frame(width: 260, height: 200)
+                            .overlay {
+                                GeometryReader { geo in
+                                    Text("使用 position")
+                                        .frame(width: 100, height: 44)
+                                        .background(.gray)
+                                        .position(x: 0, y: 100)
+//                                        .background(.purple) // 注释看看
+                                }
+                            }
+                        
+                    }
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.cyan)
+            }
+            Divider()
+        }
     }
 }
 
